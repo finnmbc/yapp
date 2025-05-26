b365a7fdb33261ffec244fed08ae606980677eea
finnmbc <finn.miebach@web.de>
2025-05-26 13:39:29 +0200

    snens

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const ROOM_DURATION_MS = 10 * 60 * 1000; // 10 Minuten
 const RESHUFFLE_INTERVAL_MS = 2 * 60 * 1000; // 2 Minuten
 
 let rooms = [];
+let nextShuffleTimestamp = Date.now() + RESHUFFLE_INTERVAL_MS;
 
 function createRoom() {
   return {
@@ -32,7 +33,6 @@ function updateRoomsForAll() {
 }
 
 function assignUserToRoom(socketId) {
-  // Finde einen Raum mit weniger als MAX_USERS_PER_ROOM Nutzern
   let room = rooms.find(r => r.users.length < MAX_USERS_PER_ROOM);
   if (!room) {
     room = createRoom();
@@ -43,17 +43,14 @@ function assignUserToRoom(socketId) {
 }
 
 function shuffleUsers() {
-  // Alle Nutzer sammeln
   const allUsers = rooms.flatMap(r => r.users);
-  rooms = []; // Räume resetten
+  rooms = [];
 
-  // Nutzer zufällig mischen
   for (let i = allUsers.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [allUsers[i], allUsers[j]] = [allUsers[j], allUsers[i]];
   }
 
-  // Nutzer neu auf Räume verteilen
   allUsers.forEach(socketId => {
     let room = rooms.find(r => r.users.length < MAX_USERS_PER_ROOM);
     if (!room) {
@@ -63,21 +60,20 @@ function shuffleUsers() {
     room.users.push(socketId);
   });
 
-  // Teilnehmer auch in Socket.io neu joinen lassen
-  // Zuerst alle Nutzer rauswerfen aus alten Räumen
   io.sockets.sockets.forEach((socket) => {
-    // Entferne den Nutzer aus allen Räumen
     const socketRooms = Array.from(socket.rooms).filter(r => r !== socket.id);
     socketRooms.forEach(rId => socket.leave(rId));
   });
 
-  // Dann jedem Nutzer neuen Raum joinen und informieren
   rooms.forEach(room => {
     room.users.forEach(socketId => {
       const socket = io.sockets.sockets.get(socketId);
       if (socket) {
         socket.join(room.id);
-        socket.emit('joined_room', room.id);
+        socket.emit('joined_room', {
+          roomId: room.id,
+          nextShuffleTimestamp
+        });
       }
     });
   });
@@ -94,7 +90,10 @@ io.on('connection', (socket) => {
 
   const room = assignUserToRoom(socket.id);
   socket.join(room.id);
-  socket.emit('joined_room', room.id);
+  socket.emit('joined_room', {
+    roomId: room.id,
+    nextShuffleTimestamp
+  });
 
   updateRoomsForAll();
 
@@ -104,21 +103,19 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
-    // Nutzer aus Raum entfernen
     rooms.forEach(r => {
       r.users = r.users.filter(u => u !== socket.id);
     });
-    // Räume ohne Nutzer löschen
     cleanupRooms();
-
     updateRoomsForAll();
   });
 });
 
-// Timer für alle 2 Minuten Räume zufällig neu mischen
+// Shuffle alle 2 Minuten
 setInterval(() => {
   console.log('Shuffle users in rooms...');
   shuffleUsers();
+  nextShuffleTimestamp = Date.now() + RESHUFFLE_INTERVAL_MS;
 }, RESHUFFLE_INTERVAL_MS);
 
 const PORT = process.env.PORT || 3000;
